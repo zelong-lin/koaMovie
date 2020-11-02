e9a4c2bb4b28aad126b45194b7cb45088c0c0d1e
zero <1243499331@qq.com>
2020-11-02 18:02:06 +0800

    引入babel

---
+++ test/dec.js
@@ -0,0 +1,24 @@
+class Boy {
+  // @speak('中文')
+  // run () {
+  //   console.log('I can speak ' + this.language)
+  //   // console.log('I can run!')
+  //   return 'aaa'
+  // }
+}
+
+function speak (language) {
+  return function (target, key, descriptor) {
+    console.log(target)
+    console.log(key)
+    console.log(descriptor)
+
+    target.language = language
+
+    return descriptor
+  }
+}
+
+const luke = new Boy()
+
+luke.run()
